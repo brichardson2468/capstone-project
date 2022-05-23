d061c4a34bddf8f774a0d70a99c58e37fbf63372
Brad Richardson <brichardson2468@gmail.com>
2022-05-23 10:21:28 -0400

    Initial file setup

---
+++ client/src/components/SignUp.js
@@ -0,0 +1,15 @@
+import { useState } from "react"
+import { useNavigate } from 'react-router-dom'
+import chalkboard from '../images/chalkboard.jpg'
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+
+
+
+
+
+
+
+
+export default SignUp
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   
+  resources :doctors, only: [:index, :create, :show, :destroy]
+  resources :patients, only: [:index, :create, :show, :destroy]
+  
+  
+  
+  
+  
+  
+  
+  
+  
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
