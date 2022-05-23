07551ef7aec3ee4bacc299ac995d1d513713301c
Brad Richardson <brichardson2468@gmail.com>
2022-05-23 16:24:20 -0400

    Started frontend

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,21 @@
 class DoctorsController < ApplicationController
+
+    def index
+        render json: Doctor.all, status: :ok
+    end
+    
+    def show
+        if params[:id]
+            doctor = Doctor.find(params[:id])
+            render json: doctor, include: :patients
+        end
+    end
+
+
+
+
+
+
+
+
 end
