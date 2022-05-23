bac2cf922ab738267b1770208113f3e4d20d9122
Brad Richardson <brichardson2468@gmail.com>
2022-05-23 17:46:55 -0400

    Frontend and backend load in browser

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,12 +1,11 @@
 class DoctorsController < ApplicationController
 
     def index
-        doctors = Doctor.all
-        render json: doctors, status: :ok
+        render json: Doctor.all, status: :ok
     end
     
     def show
-          doctor = Doctor.find(params[:id])
+        doctor = Doctor.find(params[:id])
         if doctor  
             render json: DoctorSerializer.new(doctor).as_json, status: :ok
         else
