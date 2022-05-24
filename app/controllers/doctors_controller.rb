42edc036e52e1ff4e92b204c55dcdc9deeee8e32
Brad Richardson <brichardson2468@gmail.com>
2022-05-23 21:55:03 -0400

    Added auth, login, signup on backend

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -4,12 +4,44 @@ class DoctorsController < ApplicationController
         render json: Doctor.all, status: :ok
     end
     
-    def show
-        doctor = Doctor.find(params[:id])
-        if doctor  
-            render json: DoctorSerializer.new(doctor).as_json, status: :ok
+    def create 
+        doctor = Doctor.create!(username: params[:username], password: params[:password])
+        if doctor.valid?
+            session[:doctor_id] = doctor.id
+            render json: doctor, status: :created
         else
-            render json: {message: "Doctor with id #{params[:id]} not found"}, status: 404
+            render json: doctor.errors.full_messages, status: 422
         end
     end
+    
+    def show 
+        if params[:id]
+            doctor = Doctor.find(params[:id])
+            render json: doctor 
+        end
+       
+        doctor = Doctor.find_by(id: session[:doctor_id])
+        if doctor
+            render json: doctor, status: :ok
+        else
+            render json: {error: "doctor not found"}, status: 401
+        end
+    end
+
+    def destroy
+        doctor = Doctor.find_by(id: params[:id])
+        if doctor 
+            doctor.destroy 
+            head :no_content
+        else
+            render json: {error: "doctor not found"}, status: 404
+        end
+    end
+
+    private
+
+    def doctor_params
+        params.permit(:username, :password)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
   
- 
-  
-  
-  
-  
-  
-  
-  
   resources :appointments, only: [:index, :create, :show, :destroy]
   resources :scores, only: [:index, :create, :show, :destroy]
   resources :patients, only: [:index, :create, :show, :destroy]
   resources :doctors, only: [:index, :create, :show, :destroy]
+
+  post '/login', to: "sessions#create"
+
+  delete '/logout', to: "sessions#destroy"
+
+  get '/auth', to: "doctors#show"
+
+  get '/signup', to: "doctors#create"
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
